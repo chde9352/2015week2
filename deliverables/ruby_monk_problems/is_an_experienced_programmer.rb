4ab10cb168b99e24efaaf6177b429815e2db6d61
Christian Dean <christianodean@gmail.com>
2015-02-16 16:40:44 -0700

    Update is_an_experienced_programmer.rb

--- deliverables/ruby_monk_problems/is_an_experienced_programmer.rb
+++ deliverables/ruby_monk_problems/is_an_experienced_programmer.rb
@@ -1,1 +1,4 @@
-is_an_experienced_programmer = # Fill your expression here
+is_an_experienced_ruby_programmer =
+  (candidate.languages_worked_with.include? 'Ruby') &&
+  (candidate.years_of_experience >= 2 || candidate.github_points >= 500) &&
+  ! (candidate.age < 15 || candidate.applied_recently?)
