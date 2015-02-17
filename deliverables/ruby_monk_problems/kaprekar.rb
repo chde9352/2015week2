8d2bd7c45a86266c60ac9189fbde70f78c1f4bb7
Christian Dean <christianodean@gmail.com>
2015-02-16 17:57:10 -0700

    Update kaprekar.rb

--- deliverables/ruby_monk_problems/kaprekar.rb
+++ deliverables/ruby_monk_problems/kaprekar.rb
@@ -1,2 +1,11 @@
-def kaprekar?(k)
+def location_in_hierarchy(object, method)
+  klass = object.class
+  ancestors = [klass]
+  while not (superclass = klass.superclass).nil?
+    ancestors << superclass
+    klass = superclass
+  end
+  ancestors.reverse.find do |ancestor| 
+    ancestor.instance_methods.include?(method)
+  end
 end
